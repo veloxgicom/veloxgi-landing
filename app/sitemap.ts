a45dd1ff1fcb85c25c455dfffcd4b35a66ad1361
Simasoftw <frainer2013@gmail.com>
2025-11-08 17:21:51 -0500

    [ADD] Modificarción de sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,25 +2,41 @@ import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://veloxgi.com'
+  const currentDate = new Date()
 
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: currentDate,
       changeFrequency: 'weekly',
       priority: 1,
+      alternates: {
+        languages: {
+          es: baseUrl,
+        },
+      },
     },
     {
       url: `${baseUrl}/privacy`,
-      lastModified: new Date(),
+      lastModified: currentDate,
       changeFrequency: 'monthly',
-      priority: 0.5,
+      priority: 0.8,
+      alternates: {
+        languages: {
+          es: `${baseUrl}/privacy`,
+        },
+      },
     },
     {
       url: `${baseUrl}/terms`,
-      lastModified: new Date(),
+      lastModified: currentDate,
       changeFrequency: 'monthly',
-      priority: 0.5,
+      priority: 0.8,
+      alternates: {
+        languages: {
+          es: `${baseUrl}/terms`,
+        },
+      },
     },
   ]
 }
