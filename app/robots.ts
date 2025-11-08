370362e06c15291f6df7a90b564b12b812ef6f4a
Simasoftw <frainer2013@gmail.com>
2025-11-08 17:34:22 -0500

    Prueba de sitemap.xml

--- app/robots.ts
+++ app/robots.ts
@@ -11,7 +11,7 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/api/', '/_next/'],
       },
     ],
-    sitemap: `${baseUrl}/sitemap.xml`,
+    //sitemap: `${baseUrl}/sitemap.xml`,
   }
 }
 
